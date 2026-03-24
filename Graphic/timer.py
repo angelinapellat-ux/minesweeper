61ae2f6a4e91b2141f857f7aaafd025fc09344db
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-03-24 12:20:17 +0100

    Starting Difficulty.py and Timer.py

---
+++ Graphic/timer.py
@@ -0,0 +1,9 @@
+class Timer:
+    def __init__(self, countdown, countdown_end=0):
+        self.countdown = countdown
+        self.countdown_end = countdown_end
+
+    def starting_countdown(countdown):
+
+
+    def 
