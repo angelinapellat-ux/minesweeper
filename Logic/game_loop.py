18a40deb680c8831a27d0f898a3c1434348b1e70
Yannisand <yannis.sandoval22@gmail.com>
2026-03-26 14:14:56 +0100

    update Logic files

---
+++ Logic/game_loop.py
@@ -0,0 +1,60 @@
+import random
+from tile import Tile
+
+
+def grid_setup(grid_size):
+    grid = []
+    for longitude in range(grid_size):
+        grid_line = []
+        for latitude in range(grid_size):
+            grid_line += [Tile((longitude, latitude))]
+        grid += [grid_line]
+    return grid
+
+def spawn_mine(grid, first_selection):
+    grid_size = len(grid)
+    longitude = random.randint(0, grid_size)
+    latitude = random.randint(0, grid_size)
+    test_mine = grid[longitude][latitude]
+    if test_mine.get_ismine() == True or test_mine in grid[first_selection[0]][first_selection[1]].select_around(grid, True):
+        spawn_mine(grid, first_selection)
+    else:
+        test_mine.set_mine(grid)
+
+def check_victory(grid):
+    for longitude in grid:
+        for latitude in grid[longitude]:
+            if grid[longitude][latitude].get_ismine() == False and not grid[longitude][latitude].get_status() == "Visible":
+                return False
+    return True
+
+
+def grid_display(grid):
+    for longitude in grid:
+        print(grid_line)
+
+def input_coordinate():
+    longitude = int(input("Longitude: "))
+    latitude = int(input("Latitude: "))
+    return (longitude, latitude)
+
+
+def game_loop():
+    difficulty = input("Difficulty: ")
+    grid = grid_setup(3)
+    nbmines = 2
+    first_selection = input_coordinate()
+    for loop in range(nbmines):
+        spawn_mine(grid, first_selection)
+    while True:
+        result = "Continue"
+        selection = input_coordinate()
+        result = grid[selection[0]][selection[1]].discover_tile()
+        if result == "Perdu":
+            print("Perdu")
+        elif check_victory(grid) == True:
+            print("Gagnée")
+
+
+grid = grid_setup(3)
+grid_display(grid)
---
+++ Logic/tile.py
@@ -0,0 +1,80 @@
+
+
+class Tile:
+    def __init__(self, coordinate, status="Hidden", ismine=False, nbmines=0):
+        self.coordinate = coordinate
+        self.status = status
+        self.__ismine = ismine
+        self.nbmines = nbmines
+
+    def get_coordinate(self):
+        return self.coordinate
+
+    def get_longitude(self):
+        return self.coordinate[0]
+    
+    def get_latitude(self):
+        return self.coordinate[1]
+
+    def get_status(self):
+        return self.status
+    
+    def get_ismine(self):
+        return self.__ismine
+    
+    def get_nbmines(self):
+        return self.nbmines
+
+    def safety_limit(self, coordinate, grid_size):
+        if coordinate < 0:
+            coordinate = 0
+        elif coordinate >= grid_size:
+            coordinate = grid_size - 1
+        return coordinate
+    
+    def select_around(self, grid, center, selected_tiles = []):
+        grid_size = len(grid)
+        for longitude in range(self.safety_limit(grid[self.get_longitude()-1], grid_size), 
+                               self.safety_limit(grid[self.get_longitude()+1], grid_size)):
+            for latitude in range(self.safety_limit(grid[longitude][self.get_latitude()-1], grid_size), 
+                                  self.safety_limit(grid[longitude][self.get_latitude()+1], grid_size)):
+                if center == True or not grid[longitude][latitude] == self:
+                    selected_tiles += [grid[longitude][latitude]]
+        selected_tiles -= selected_tiles[4]
+        return selected_tiles
+
+    def set_status(self):
+        if self.get_status() == "Hidden":
+            self.status == "Flag"
+        elif self.get_status() == "Flag":
+            self.status == "Mystery"
+        elif self.get_status() == "Mystery":
+            self.status == "Hidden"
+
+    def reveal_tile(self, lose):
+        if lose == True:
+            self.status = "Kaboom"
+        else:
+            self.status = "Visible"
+
+    def set_mine(self, grid):
+        self.ismine = True
+        tiles_around = self.select_around(grid, False)
+        for selected_tile in tiles_around:
+            selected_tile.add_mine()
+
+    def add_mine(self):
+        self.nbmines += 1
+        
+    def discover_tile(self, grid):
+        if self.get_ismine() == True:
+            self.reveal_tile(True)
+            return "Lose"
+        elif self.get_nbmines() == 0:
+            self.reveal_tile(False)
+            tiles_around = self.select_around(grid, False)
+            for selected_tiles in tiles_around:
+                if selected_tiles.get_status() == "Hidden":
+                    selected_tiles.discover_tile(grid)
+        else:
+            self.reveal_tile(False)
