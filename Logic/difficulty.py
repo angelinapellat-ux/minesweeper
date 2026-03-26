bcd618251f6a362bbc63a404d93142df28440aa8
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-03-26 14:59:28 +0100

    Added Timer and Difficulty to Main (not finished)

---
+++ Logic/difficulty.py
@@ -0,0 +1,24 @@
+
+
+DIFFICULTY_CONFIG = {
+    "EASY": {
+        "MAX_MINES" : 12,
+        "MAX_TILES" : 5*5,
+        "MAX_TIMER" : 120
+    },
+    "NORMAL": {
+        "MAX_MINES" : 30,
+        "MAX_TILES" : 7*7,
+        "MAX_TIMER" : 240
+    },
+    "HARD": {
+        "MAX_MINES" : 45,
+        "MAX_TILES" : 9*9,
+        "MAX_TIMER" : 360
+    },
+    "IMPOSSIBLE": {
+        "MAX_MINES" : 7,
+        "MAX_TILES" : 3*3,
+        "MAX_TIMER" : 20
+    }
+}
