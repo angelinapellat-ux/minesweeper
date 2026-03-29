a95bf0b21140fbd67b4367ecbb596a4d8bfa93d0
Yannisand <yannis.sandoval22@gmail.com>
2026-03-29 22:24:41 +0200

    feat : fix logic bugs

--- Logic/game_loop.py
+++ Logic/game_loop.py
@@ -2,25 +2,34 @@ import random
 from tile import Tile
 
 
+# Crée la grille sous forme d'instances de clase dans des listes dans des listes
 def grid_setup(grid_size):
     grid = []
+    # Crée autant de sous-listes que de lignes dans la grille
     for longitude in range(grid_size):
         grid_line = []
+        # Crée autant d'instances de case que de colonnes dans la grille
         for latitude in range(grid_size):
             grid_line += [Tile((longitude, latitude))]
         grid += [grid_line]
     return grid
 
+# Pose au hasard les mines en début de partie
 def spawn_mine(grid, first_selection):
     grid_size = len(grid)
     longitude = random.choice(grid)
     latitude = random.choice(longitude)
     test_mine = latitude
-    if test_mine.get_ismine() == True or test_mine in grid[first_selection[0]][first_selection[1]].select_around(grid, True):
+    safe_start_zone = [grid[first_selection[0]][first_selection[1]]] + grid[first_selection[0]][first_selection[1]].select_around(grid)
+    # Rappelle la fonction s'il y a déjà une mine sur l'emplacement choisi ou si la mine sera placé trop proche de la première sélection
+    if test_mine.get_ismine() == True or test_mine in safe_start_zone:
         spawn_mine(grid, first_selection)
+        print("spawn_mine rappelé")
     else:
         test_mine.set_mine(grid)
+        print(f"mine placé à {test_mine.get_coordinate()}")
 
+# Vérifie si toutes les case sans mines sont découvertes pour déclarer la victoire
 def check_victory(grid):
     for longitude in grid:
         for latitude in longitude:
@@ -28,32 +37,55 @@ def check_victory(grid):
                 return False
     return True
 
-
+# Affiche la grille ligne par ligne sous forme de symbole
 def grid_display(grid):
     for longitude in grid:
-        print("grid_line")
+        grid_line = []
+        for latitude in longitude:
+            # L'état de la case et sont contenue sont tout deux affichés pour faciliter la programmation
+            if latitude.get_status() == "Hidden":
+                symbol = "-"
+            elif latitude.get_status() == "Flag":
+                symbol = "F"
+            elif latitude.get_status() == "Mystery":
+                symbol = "?"
+            elif latitude.get_status() == "Visible":
+                symbol = " "
+            elif latitude.get_status() == "Kaboom":
+                symbol = "X"
+            if latitude.get_ismine() == True:
+                symbol += "x"
+            else:
+                symbol += f"{latitude.get_nbmines()}"
+            grid_line += [symbol]
+        print(grid_line)
 
+# Demande les coordonnée de la case sélectionnée par le joueur
 def input_coordinate():
     longitude = int(input("Longitude: "))
     latitude = int(input("Latitude: "))
     return (longitude-1, latitude-1)
 
+# Demande si la case sélectionnée doit être découverte ou changer d'état
 def input_action():
-    action = input("Découvrir (D) ; Changer l'état (E)")
+    action = input("Découvrir (D) ; Changer l'état (E) : ")
     return action
 
-
+# Jeu
 def game():
+    # Difficulté pas encore implémenté
     difficulty = input("Difficulty: ")
-    grid = grid_setup(5)
-    nbmines = 4
+    grid = grid_setup(10)
+    nbmines = 6
     grid_display(grid)
     first_selection = input_coordinate()
+    # Pose le nombre de mines définie pour la partie
     for loop in range(nbmines):
         spawn_mine(grid, first_selection)
     grid[first_selection[0]][first_selection[1]].discover_tile(grid)
-    grid_display(grid)
+    # Boucle principale du jeu
     while True:
+        grid_display(grid)
         result = "Continue"
         selection = input_coordinate()
         action = input_action()
@@ -61,12 +93,15 @@ def game():
             result = grid[selection[0]][selection[1]].discover_tile(grid)
             if result == "Flag":
                 print("Un drapeau ne peut pas être découvert, veuillez enlever le drapeau")
-            elif result == "Perdu":
+            elif result == "Lose":
                 print("Perdu")
+                grid_display(grid)
+                break
             elif check_victory(grid) == True:
                 print("Gagnée")
+                grid_display(grid)
+                break
         elif action == "E":
             grid[selection[0]][selection[1]].set_status()
-        grid_display(grid)
 
 game()
--- Logic/tile.py
+++ Logic/tile.py
@@ -1,3 +1,5 @@
+
+# Crée une case de la grille pouvant garder ses propres information
 class Tile:
     def __init__(self, coordinate, status="Hidden", ismine=False, nbmines=0):
         self.coordinate = coordinate
@@ -23,60 +25,79 @@ class Tile:
     def get_nbmines(self):
         return self.nbmines
 
+# S'assure que les coordonnées utilisé ne dépassent pas hors de la grille
     def safety_limit(self, coordinate, grid_size):
-        if coordinate < 0:
-            coordinate = 0
-        elif coordinate >= grid_size:
-            coordinate = grid_size - 1
-        print(coordinate)
-        return coordinate
-    
-    def select_around(self, grid, center, selected_tiles = []):
+        list_coordinates = []
+        if not coordinate - 1 < 0:
+            list_coordinates += [coordinate - 1]
+        list_coordinates += [coordinate]
+        if not coordinate + 1 >= grid_size:
+            list_coordinates += [coordinate + 1]
+        print(list_coordinates)
+        return list_coordinates
+
+# Sélectionne les cases autour de l'instance utilisée pour d'autres modules
+    def select_around(self, grid):
+        selected_tiles = []
         grid_size = len(grid)
-        for longitude in range(self.safety_limit(self.get_longitude()-1, grid_size), 
-                               self.safety_limit(self.get_longitude()+2, grid_size)):
-            for latitude in range(self.safety_limit(self.get_latitude()-1, grid_size), 
-                                  self.safety_limit(self.get_latitude()+2, grid_size)):
-                if center == True or not grid[longitude][latitude] == self:
-                    selected_tiles += [grid[longitude][latitude]]
-                    for i in selected_tiles:
-                        print(i.get_coordinate())
+        longitude_coordinates = self.safety_limit(self.get_longitude(), grid_size)
+        latitude_coordinates = self.safety_limit(self.get_latitude(), grid_size)
+        for longitude in longitude_coordinates:
+            for latitude in latitude_coordinates:
+                # Récupère toutes les instances de case dans la zone sauf elle-même
+                if not grid[longitude][latitude] == self:
+                    selected_tiles.append(grid[longitude][latitude])
+                    print(grid[longitude][latitude].get_coordinate())
         return selected_tiles
 
+# Changer l'état des cases cachées entre "Caché", "Drapeau" et "Mystère"
     def set_status(self):
+        print(f"Ancien état : {self.get_status()}")
         if self.get_status() == "Hidden":
-            self.status == "Flag"
+            self.status = "Flag"
         elif self.get_status() == "Flag":
-            self.status == "Mystery"
+            self.status = "Mystery"
         elif self.get_status() == "Mystery":
-            self.status == "Hidden"
+            self.status = "Hidden"
+        else:
+            print("Cette case est déjà découverte")
+        print(f"Nouvel état : {self.get_status()}")
 
+# Affiche le contenu de la case
     def reveal_tile(self, lose):
         if lose == True:
             self.status = "Kaboom"
         else:
             self.status = "Visible"
 
+# Pose une mine
     def set_mine(self, grid):
-        self.ismine = True
-        tiles_around = self.select_around(grid, False)
+        self.__ismine = True
+        print(f"{self.get_coordinate()} est maintenant une mine")
+        # Sélectionne les case autour pour augmenter leur compteur de mines
+        tiles_around = self.select_around(grid)
         for selected_tile in tiles_around:
             selected_tile.add_mine()
 
+# Ajoute une mine au compteur de mines autour de l'instance de case
     def add_mine(self):
         self.nbmines += 1
-        
+
+# Révèle le contenue de la case sélectionnée et réagit en conséquence
     def discover_tile(self, grid):
         if self.get_status() == "Flag":
             return "Flag"
+        # Renvoie "Perdu" si la case est une mine
         elif self.get_ismine() == True:
             self.reveal_tile(True)
             return "Lose"
+        # Si la case n'a pas de mines autour, elle sélectionne les cases autours pour les révéler leur contenue sauf si la case est déjà révélée
         elif self.get_nbmines() == 0:
             self.reveal_tile(False)
-            tiles_around = self.select_around(grid, False)
+            tiles_around = self.select_around(grid)
             for selected_tiles in tiles_around:
                 if selected_tiles.get_status() == "Hidden":
                     selected_tiles.discover_tile(grid)
+        # Si la case a des mines autour, elle ne fait que se découvrir
         else:
             self.reveal_tile(False)
